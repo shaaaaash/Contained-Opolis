51ab504bbdb6fab9a2d44f7920af072301197989
shaaaaash <kerikmar@gmail.com>
2026-08-06 14:37:41 +0300

    fixing an issue with new line not being rendered properly in tooltips

--- kubejs/client_scripts/ItemTooltips.js
+++ kubejs/client_scripts/ItemTooltips.js
@@ -5,4 +5,22 @@ ItemEvents.modifyTooltips(event => {
     event.modify('oritech:fluxite', tooltip => {
         tooltip.removeLine(1);
     });
+
+    event.addAdvanced(Ingredient.all, (item, advanced, text) => {
+        for (let i = 0; i < text.size(); i++) {
+            let line = text.get(i)
+            let str = line.getString()
+            
+            if (str.contains('\n')) {
+                let parts = str.split('\n')
+                
+                text.set(i, Text.of(parts[0]).style(line.style))
+                
+                for (let j = 1; j < parts.length; j++) {
+                    text.add(i + j, Text.of(parts[j]).style(line.style))
+                }
+            }
+        }
+    });
+
 })
